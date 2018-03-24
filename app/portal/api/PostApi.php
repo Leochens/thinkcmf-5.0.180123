fcd3792fe3adc3acf3ff85c8ac16d73e1320bf1e
Leochens <18332518328@163.com>
2018-03-24 15:37:19 +0800

    最新栏目处理完成

--- app/portal/api/PostApi.php
+++ app/portal/api/PostApi.php
@@ -6,18 +6,34 @@
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: Leochens
+// | Author: 张鹤麟
 // +----------------------------------------------------------------------
-namespace app\portal\controller;
+namespace app\portal\api;
 
-use cmf\controller\HomeBaseController;
 use app\portal\model\PortalPostModel;
-class IndexController extends HomeBaseController
+
+class PostApi
 {
+    
+
+    /**
+     * 页面列表 用于导航选择
+     * @return array
+     */
     public function index()
     {
-        
-        return $this->fetch(':index');
+        $portalPostModel = new PortalPostModel();
 
+        $where = [
+            //'post_type'      => 2,
+            'published_time' => [['< time', time()], ['> time', 0]],
+            'post_status'    => 1,
+            'delete_time'    => 0
+        ];
+
+
+        $pages = $portalPostModel->field('id,post_title AS name')->where($where)->select();
+        return $pages;
     }
+
 }
